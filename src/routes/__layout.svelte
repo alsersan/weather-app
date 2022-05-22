f0bf7ed9cba603f6f46d4820d07b19bac9ebfc96
Álvaro Serrano <alsersan20@gmail.com>
2022-05-22 19:06:18 +0200

    Add basic layout using iphone image

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,31 @@
 <script>
 	import '../app.css';
+	import image from '$assets/images/iphone12mini.png';
 </script>
 
-<slot />
+<main>
+	<img class="iphone-image" src={image} alt="bla" />
+	<div class="content"><slot /></div>
+</main>
+
+<style lang="postcss">
+	main {
+		position: relative;
+
+		.iphone-image {
+			width: 420px;
+		}
+		.content {
+			/* screen size of iphone 12 mini */
+			width: 375px;
+			height: 812px;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			z-index: -1;
+			overflow: hidden;
+			border-radius: 20px;
+		}
+	}
+</style>
