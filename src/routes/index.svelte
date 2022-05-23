7f6e9e99f63d3ed766e718590c90ed1b062301ec
Álvaro Serrano <alsersan20@gmail.com>
2022-05-23 19:50:03 +0200

    Display icon in current weather section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,28 +1,46 @@
 <script lang="ts">
 	import { getRealtimeWeather } from '$services/get-weather.service';
+	import sunCloud from '$assets/icons/sun-cloud.webp';
 
-	const weatherApiData = getRealtimeWeather();
+	const currentWeatherData = getRealtimeWeather();
 </script>
 
-<div class="container">
-	{#await weatherApiData then weather}
-		<h1>{weather.city}</h1>
-		<img src={weather.iconUrl} alt={`${weather.description} icon`} />
-		<span>{`${weather.description}`}</span>
-		<span>{`${weather.temperature} ºC`}</span>
-		<span>{`${weather.windSpeed} km/h`}</span>
-		<span>{`${weather.humidity} %`}</span>
-		<span>Is daytime: {weather.isDaytime}</span>
-	{/await}
+<div class="weather-container">
+	<section class="current-weather">
+		{#await currentWeatherData then weather}
+			<h1>{weather.city}</h1>
+			<img class="weather-icon" src={sunCloud} alt={`${weather.description} icon`} />
+			<span>{`${weather.description}`}</span>
+			<span>{`${weather.temperature} ºC`}</span>
+			<span>{`${weather.windSpeed} km/h`}</span>
+			<span>{`${weather.humidity} %`}</span>
+			<span>Is daytime: {weather.isDaytime}</span>
+		{/await}
+	</section>
+	<section class="weather-forecast" />
 </div>
 
 <style lang="postcss">
-	.container {
+	.weather-container {
 		width: 100%;
 		height: 100%;
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+
+		.current-weather {
+			height: 60%;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+
+			.weather-icon {
+				height: 150px;
+				filter: drop-shadow(10px 10px 20px rgba(0, 0, 0, 0.2));
+			}
+		}
+
+		.weather-forecast {
+			height: 40%;
+		}
 	}
 </style>
