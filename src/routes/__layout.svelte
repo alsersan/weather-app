d3e4a2fb511c2cba095d16ca5b881c611d620efc
Álvaro Serrano <alsersan20@gmail.com>
2022-05-23 20:55:30 +0200

    Modify layout and add background layer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,6 +5,7 @@
 
 <main>
 	<img class="iphone-image" src={image} alt="bla" />
+	<div class="background-layer" />
 	<div class="content">
 		<div class="notch" />
 		<slot />
@@ -14,27 +15,44 @@
 <style lang="postcss">
 	main {
 		position: relative;
+		height: 845px;
+		width: 420px;
+	}
 
-		.iphone-image {
-			width: 420px;
-		}
-		.content {
-			/* screen size of iphone 12 mini */
-			width: 375px;
-			height: 812px;
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			z-index: -1;
-			overflow: hidden;
-			border-radius: 20px;
-			display: flex;
-			flex-direction: column;
-		}
+	.iphone-image {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		z-index: 2;
+	}
 
-		.notch {
-			height: 4.5%;
-		}
+	.background-layer {
+		/* screen size of iphone 12 mini */
+		width: 375px;
+		height: 812px;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		border-radius: 45px;
+		z-index: 1;
+		background-color: yellow;
+	}
+
+	.notch {
+		height: 4.5%;
+	}
+	.content {
+		width: 370px;
+		height: 804px;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		z-index: 3;
+		overflow: hidden;
+		border-radius: 45px;
+		display: flex;
+		flex-direction: column;
 	}
 </style>
