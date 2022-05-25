56eb07d989dae4b77a04c0505e7a5eae4ac7f382
Álvaro Serrano <alsersan20@gmail.com>
2022-05-25 19:17:19 +0200

    Refactor notch to a separate component

---
+++ src/components/notch.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { Signal } from 'lucide-svelte';
+	import { BatteryFull } from 'lucide-svelte';
+</script>
+
+<div class="notch">
+	<div class="notch__icon notch__icon--left">
+		<Signal size={25} />
+	</div>
+
+	<div class="notch__icon notch__icon--right">
+		<BatteryFull size={25} />
+	</div>
+</div>
+
+<style lang="postcss">
+	.notch {
+		height: 4.5%;
+		position: relative;
+
+		&__icon {
+			position: absolute;
+			top: 50%;
+			transform: translateY(-50%);
+
+			&--left {
+				left: 7%;
+			}
+
+			&--right {
+				right: 7%;
+			}
+		}
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,6 @@
 <script>
-	import { Signal } from 'lucide-svelte';
-	import { BatteryFull } from 'lucide-svelte';
 	import '../app.css';
+	import Notch from '$components/notch.svelte';
 	import phone from '$assets/images/iphone12mini.png';
 </script>
 
@@ -9,15 +8,7 @@
 	<img class="iphone-image" src={phone} alt="Iphone 12 mini frame" />
 	<div class="background-layer" />
 	<div class="content">
-		<div class="notch">
-			<div class="icon icon-left">
-				<Signal size="25" />
-			</div>
-
-			<div class="icon icon-right">
-				<BatteryFull size="25" />
-			</div>
-		</div>
+		<Notch />
 		<slot />
 	</div>
 </main>
@@ -49,10 +40,6 @@
 		background-color: yellow;
 	}
 
-	.notch {
-		height: 4.5%;
-		position: relative;
-	}
 	.content {
 		width: 370px;
 		height: 804px;
@@ -65,19 +52,5 @@
 		border-radius: 45px;
 		display: flex;
 		flex-direction: column;
-	}
-
-	.icon {
-		position: absolute;
-		top: 50%;
-		transform: translateY(-50%);
-
-		&-left {
-			left: 7%;
-		}
-
-		&-right {
-			right: 7%;
-		}
 	}
 </style>
