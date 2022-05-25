41b8a68bfda5e617965b0325e2b4519949364adb
Álvaro Serrano <alsersan20@gmail.com>
2022-05-25 19:09:56 +0200

    Add battery and signal icon to notch

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { Signal } from 'lucide-svelte';
+	import { BatteryFull } from 'lucide-svelte';
 	import '../app.css';
 	import phone from '$assets/images/iphone12mini.png';
 </script>
@@ -7,7 +9,15 @@
 	<img class="iphone-image" src={phone} alt="Iphone 12 mini frame" />
 	<div class="background-layer" />
 	<div class="content">
-		<div class="notch" />
+		<div class="notch">
+			<div class="icon icon-left">
+				<Signal size="25" />
+			</div>
+
+			<div class="icon icon-right">
+				<BatteryFull size="25" />
+			</div>
+		</div>
 		<slot />
 	</div>
 </main>
@@ -41,6 +51,7 @@
 
 	.notch {
 		height: 4.5%;
+		position: relative;
 	}
 	.content {
 		width: 370px;
@@ -54,5 +65,19 @@
 		border-radius: 45px;
 		display: flex;
 		flex-direction: column;
+	}
+
+	.icon {
+		position: absolute;
+		top: 50%;
+		transform: translateY(-50%);
+
+		&-left {
+			left: 7%;
+		}
+
+		&-right {
+			right: 7%;
+		}
 	}
 </style>
