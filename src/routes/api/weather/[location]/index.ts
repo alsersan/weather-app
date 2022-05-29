8ea0c35d5bfd90e67e52cce6bc229e2b7bfa7f22
Álvaro Serrano <alsersan20@gmail.com>
2022-05-29 19:06:33 +0200

    Implement weather forecast  in weather endpoint

--- src/routes/api/weather/[location]/index.ts
+++ src/routes/api/weather/[location]/index.ts
@@ -1,5 +1,11 @@
 import type { RequestHandler } from '@sveltejs/kit';
-import type { WeatherData, ApiData } from '$models/weather-data.model';
+import type {
+	ApiData,
+	DayWeather,
+	HourWeather,
+	DayForecast,
+	WeatherForecast
+} from '$models/weather-data.model';
 
 type Params = { location: string };
 type OutputType = ApiData;
@@ -17,12 +23,49 @@ export const get: RequestHandler<Params, OutputType> = async ({ params }) => {
 		options
 	)
 		.then((res) => res.json())
-		.then((res) => {
-			console.log(res);
+		.then((res): ApiData => {
 			const { location, current, forecast } = res;
+
+			// Current weather
 			const { condition, feelslike_c, humidity, temp_c, wind_kph, is_day } = current;
 			const { text, icon } = condition;
 			const { country, name, region, localtime } = location;
+
+			//Weather forecast
+			const { forecastday } = forecast;
+			const weatherForecast: WeatherForecast = forecastday.map((day: any): DayForecast => {
+				const { day: dayWeather, hour: hourWeather } = day;
+				const processedDayWeather: DayWeather = {
+					description: dayWeather.condition.text,
+					iconUrl: dayWeather.condition.icon,
+					code: dayWeather.condition.code,
+					maxTemperature: dayWeather.maxtemp_c,
+					minTemperature: dayWeather.mintemp_c,
+					averageTemperature: dayWeather.avgtemp_c,
+					chanceOfRain: dayWeather.daily_chance_of_rain,
+					chanceOfSnow: dayWeather.daily_chance_of_snow,
+					averageHumidity: dayWeather.avghumidity,
+					maxWindSpeed: dayWeather.maxwind_kph,
+					uvIndex: dayWeather.uv
+				};
+				const processedHourWeather: HourWeather[] = hourWeather.map(
+					(hour: any): HourWeather => ({
+						time: hour.time,
+						iconUrl: hour.condition.icon,
+						code: hour.condition.code,
+						temperature: hour.temperature,
+						uvIndex: hour.uv,
+						chanceOfRain: hour.chance_of_rain
+					})
+				);
+				return {
+					date: day.date,
+					day: processedDayWeather,
+					hour: processedHourWeather
+				};
+			});
+
+			// Complete weather object
 			return {
 				current: {
 					city: name,
@@ -36,8 +79,9 @@ export const get: RequestHandler<Params, OutputType> = async ({ params }) => {
 					humidity,
 					windSpeed: wind_kph,
 					isDaytime: Boolean(is_day)
-				} as WeatherData
-			} as ApiData;
+				},
+				forecast: weatherForecast
+			};
 		});
 	return {
 		status: 200,
