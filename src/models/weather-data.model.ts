7c6b0eec403c214db099a168f6523275a830b345
Álvaro Serrano <alsersan20@gmail.com>
2022-05-29 19:02:34 +0200

    Create types for weather forecast

--- src/models/weather-data.model.ts
+++ src/models/weather-data.model.ts
@@ -1,5 +1,6 @@
 export type ApiData = {
 	current: WeatherData;
+	forecast: WeatherForecast;
 };
 
 export type WeatherData = {
@@ -14,4 +15,35 @@ export type WeatherData = {
 	humidity: number;
 	windSpeed: number;
 	isDaytime: boolean;
+};
+
+export type WeatherForecast = DayForecast[];
+
+export type DayForecast = {
+	date: string;
+	day: DayWeather;
+	hour: HourWeather[];
+};
+
+export type DayWeather = {
+	description: string;
+	iconUrl: string;
+	code: number;
+	maxTemperature: number;
+	minTemperature: number;
+	averageTemperature: number;
+	chanceOfRain: number;
+	chanceOfSnow: number;
+	averageHumidity: number;
+	maxWindSpeed: number;
+	uvIndex: number;
+};
+
+export type HourWeather = {
+	time: string;
+	iconUrl: string;
+	code: number;
+	temperature: number;
+	uvIndex: number;
+	chanceOfRain: number;
 };
