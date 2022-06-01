0a58997496339b91592a05fa9686727c55f6021c
Álvaro Serrano <alsersan20@gmail.com>
2022-06-01 15:52:12 +0200

    Create location  endpoint

---
+++ src/routes/api/location/[query]/index.ts
@@ -0,0 +1,38 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import type { Location } from '$models/location.model';
+
+type Params = { query: string };
+type OutputType = Location[];
+
+export const get: RequestHandler<Params, OutputType> = async ({ params }) => {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+			'X-RapidAPI-Key': import.meta.env.VITE_RAPIDAPI_KEY
+		}
+	};
+	const response = await fetch(
+		`https://weatherapi-com.p.rapidapi.com/search.json?q=${params.query}`,
+		options
+	)
+		.then((res) => res.json())
+		.then((res): Location[] => {
+			const locations: Location[] = res.map((location: any): Location => {
+				const { name, region, country, lat, lon } = location;
+				return {
+					city: name,
+					region,
+					country,
+					latitude: lat,
+					longitude: lon
+				};
+			});
+			return locations;
+		});
+
+	return {
+		status: 200,
+		body: response
+	};
+};
