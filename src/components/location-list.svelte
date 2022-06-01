37af5cd3bd238fbbc3afac7bb82edb85ae1176fb
Álvaro Serrano <alsersan20@gmail.com>
2022-06-01 17:35:29 +0200

    Create LocationList as a separate component

---
+++ src/components/location-list.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Locations } from '$models/location.model';
+	import { goto } from '$app/navigation';
+	import { location } from '$store';
+
+	export let locations: Locations;
+
+	const onClick = (city: string) => {
+		location.set(city);
+		goto('/');
+	};
+</script>
+
+{#each locations as location}
+	<div on:click={() => onClick(location.city)}>{location.city} {location.region}</div>
+{/each}
--- src/routes/location-search.svelte
+++ src/routes/location-search.svelte
@@ -1,24 +1,19 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
 	import { getLocation } from '$services/get-location.service';
-	import { location } from '$store';
 	import type { Locations } from '$models/location.model';
 	import type { WithTarget } from '$models/with-target.model';
+	import LocationList from '$components/location-list.svelte';
 
-	let data: Locations;
+	let data: Promise<Locations>;
 	let timer: ReturnType<typeof setTimeout>;
 	let query: string;
 
-	const onClick = (city: string) => {
-		location.set(city);
-		goto('/');
-	};
 	const debounce = (e: WithTarget<KeyboardEvent, HTMLInputElement>) => {
 		const value = e.currentTarget.value;
 		if (value !== '') {
 			clearTimeout(timer);
 			timer = setTimeout(async () => {
-				data = await getLocation(value);
+				data = getLocation(value);
 			}, 250);
 		}
 	};
@@ -26,8 +21,10 @@
 
 <label for="location-search">Enter location</label>
 <input id="location-search" bind:value={query} on:keyup={debounce} />
-{#if data}
-	{#each data as location}
-		<div on:click={() => onClick(location.city)}>{location.city} {location.region}</div>
-	{/each}
-{/if}
+{#await data}
+	bla
+{:then locations}
+	{#if locations}
+		<LocationList {locations} />
+	{/if}
+{/await}
