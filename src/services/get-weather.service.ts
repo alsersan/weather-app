5f26a912563e42eac2bcb612679c09ed8808f901
Álvaro Serrano <alsersan20@gmail.com>
2022-06-01 15:52:30 +0200

    Create getLocation service

--- src/services/get-weather.service.ts
+++ src/services/get-weather.service.ts
@@ -1,5 +1,10 @@
 import type { ApiData } from '$models/weather-data.model';
+import type { Location } from '$models/location.model';
 
 export function getWeather(location: string): Promise<ApiData> {
 	return fetch(`/api/weather/${location}`).then((data) => data.json());
+}
+
+export function getLocation(query: string): Promise<Location> {
+	return fetch(`/api/location/${query}`).then((data) => data.json());
 }
