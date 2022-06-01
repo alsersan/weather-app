7d9ddbbcf9d45f4dbbe76d83d9b7ef25add7f724
Álvaro Serrano <alsersan20@gmail.com>
2022-06-01 17:24:19 +0200

    Add input and debounce api location search

--- src/routes/location-search.svelte
+++ src/routes/location-search.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { getLocation } from '$services/get-location.service';
-	import { onMount } from 'svelte';
 	import { location } from '$store';
 	import type { Locations } from '$models/location.model';
+	import type { WithTarget } from '$models/with-target.model';
 
 	let data: Locations;
-	onMount(async () => {
-		data = await getLocation('Madrid');
-	});
+	let timer: ReturnType<typeof setTimeout>;
+	let query: string;
 
 	const onClick = (city: string) => {
 		location.set(city);
 		goto('/');
 	};
+	const debounce = (e: WithTarget<KeyboardEvent, HTMLInputElement>) => {
+		const value = e.currentTarget.value;
+		if (value !== '') {
+			clearTimeout(timer);
+			timer = setTimeout(async () => {
+				data = await getLocation(value);
+			}, 250);
+		}
+	};
 </script>
 
+<label for="location-search">Enter location</label>
+<input id="location-search" bind:value={query} on:keyup={debounce} />
 {#if data}
 	{#each data as location}
 		<div on:click={() => onClick(location.city)}>{location.city} {location.region}</div>
